3b6c37596d7db2e74761be218454f92a868530e1
Kappa <pingpongball72@gmail.com>
2022-10-25 21:41:04 -0400

    Messed with GOE, did some other stuff too I think

--- src/GameObjectsEditor.py
+++ src/GameObjectsEditor.py
@@ -9,11 +9,26 @@ def addGameObject():
 
     game.gameObjects.append(f"NAME:{ObjectName}, X:{ObjectX}, Y:{ObjectY}")
 
+def callback(sender, app_data):
+    print('OK was clicked.')
+    print("Sender: ", sender)
+    print("App Data: ", app_data)
+
+def cancel_callback(sender, app_data):
+    print('Cancel was clicked.')
+    print("Sender: ", sender)
+    print("App Data: ", app_data)
+
+def open_sprite_picker():
+    gui.add_file_dialog(directory_selector=True, show=True, callback=callback, tag="SpriteImage", 
+        cancel_callback=cancel_callback)
+
 def createGOE():
     with gui.window(label="Game Object Editor", width=450, height=150, pos=(10, 10)):
         gui.add_text("Game Object")
         gui.add_input_text(tag="ObjName", label="Object Name", source="string_value")
         gui.add_input_text(tag="ObjX", label="X", source="float_value")
         gui.add_input_text(tag="ObjY", label="Y", source="float_value")
+        #gui.add_button(label="sprite", callback=open_sprite_picker)
         gui.add_button(label="add", callback=addGameObject)
 
